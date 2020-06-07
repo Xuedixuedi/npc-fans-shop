04620afc1b696ad8a54825dcd6bbfca3856662cf
Xuedixuedi <958190137@qq.com>
2020-06-07 20:59:26 +0800

    finish front

--- js/login.js
+++ js/login.js
@@ -1,3 +1,18 @@
-$(".message a").click(function () {
-    $("form").animate({ height: "toggle", opacity: "toggle" }, "slow")
-})
+// $(".message a").click(function () {
+//     $("form").animate({ height: "toggle", opacity: "toggle" }, "slow")
+// })
+
+// var changeviewDOM = document.querySelectorAll(".message a")
+
+function changeview(text) {
+    //得到两张表
+    form = document.getElementsByTagName("form")
+    console.log(text)
+    if (text == "Sign In") {
+        form[0].style.display = "none"
+        form[1].style.display = "block"
+    } else if (text == "Create an account") {
+        form[1].style.display = "none"
+        form[0].style.display = "block"
+    }
+}
---
+++ js/menu.js
@@ -0,0 +1,51 @@
+var $menu = document.querySelector(".menu") //菜单
+var $overlay = document.querySelector(".overlay")
+
+var $cartButton = document.querySelector(".links").getElementsByTagName("li")[0] //购物车元素
+var $cartCard = document.getElementById("cart") //购物车卡片
+var $cartSettle = document.getElementById("cart").querySelectorAll(".button")[0] //结算购物车
+var $cartClose = document.getElementById("cart").querySelectorAll(".button")[1] //关闭购物车
+
+var $orderButton = document
+    .querySelector(".links")
+    .getElementsByTagName("li")[1] //订单记录
+var $orderCard = document.getElementById("order") //订单记录卡片
+var $orderClose = document
+    .getElementById("order")
+    .querySelectorAll(".button")[0] //关闭订单记录
+
+console.log($cartClose)
+
+//打开购物车
+$cartButton.addEventListener("click", function () {
+    $cartCard.style.display = "flex"
+})
+
+//结算购物车
+$cartSettle.addEventListener("click", function () {
+    $cartSettle.style.display = "none"
+    //结算逻辑
+})
+
+// 关闭购物车
+$cartClose.addEventListener("click", function () {
+    $cartCard.style.display = "none"
+})
+
+//打开订单记录
+$orderButton.addEventListener("click", function () {
+    $orderCard.style.display = "flex"
+})
+
+// 关闭订单记录
+$orderClose.addEventListener("click", function () {
+    $orderCard.style.display = "none"
+})
+
+//menu js
+$overlay.addEventListener("mouseenter", function (event) {
+    $menu.classList.add("active")
+})
+$menu.addEventListener("mouseleave", function (event) {
+    $menu.classList.remove("active")
+})
